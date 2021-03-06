56fb634a52869f9f19cbcc6a6564bdd2dcf5685f
Itay <eizzo77@gmail.com>
2021-03-07 00:42:42 +0200

    new commit

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const covidEndPoint = `https://corona-api.com/countries`;
-const countryEndPoint = `https://restcountries.herokuapp.com/api/v1`;
+// setting up some consts like buttons and countries global Obj.
 const continentsButtons = document
   .querySelector(".continent-buttons-container ")
   .querySelectorAll("input[type='button']");
@@ -7,7 +6,8 @@ const databuttons = document
   .querySelector(".data-buttons-container ")
   .querySelectorAll("input[type='button']");
 const countriesContainer = document.querySelector(".countries-container");
-
+const countriesFixedObj = {};
+// duplicate of fetch methods because somewhat it didn't work when a string was passed as an argument.
 const fetchCountry = async () => {
   const response = await fetch(
     `https://api.codetabs.com/v1/proxy?quest=https://restcountries.herokuapp.com/api/v1`
@@ -23,7 +23,7 @@ const fetchCovidData = async () => {
   const countryData = await response.json();
   return countryData.data;
 };
-
+// setting up click event handlers for buttons
 const addClickHandlers = (buttonsGroup) => {
   buttonsGroup.forEach((button) =>
     button.addEventListener("click", (e) => {
@@ -60,11 +60,13 @@ const addClickListenersDisplayCountries = () => {
     button.addEventListener("click", (e) => {
       removeAllCountries();
       const countries = getCountriesByContinent(e.target.value);
-      console.log(countries);
       countries.forEach((c) => {
         const countryEl = document.createElement("button");
         countryEl.textContent = c.name;
         countriesContainer.append(countryEl);
+        countryEl.addEventListener("click", (e) => {
+          displayCountryChart(c);
+        });
       });
     });
   });
@@ -76,21 +78,18 @@ const removeAllCountries = () => {
   }
 };
 
-const countriesFixedObj = {};
+// excecuting the fetching and setting ups.
 fetchCovidData().then((covidCountriesStats) => {
   covidCountriesStats.forEach((_, i, arr) => {
     countriesFixedObj[arr[i].code] = arr[i];
   });
-  console.log(countriesFixedObj);
   fetchCountry()
     .then((countries) => {
-      console.log(countries);
       countries.forEach((c) => {
         if (countriesFixedObj.hasOwnProperty(c.cca2)) {
           countriesFixedObj[c["cca2"]].region = c.region;
         }
       });
-      console.log(countriesFixedObj);
     })
     .then(() => {
       addClickHandlers(continentsButtons);
@@ -113,14 +112,76 @@ const getDataByCountries = (countries, dataRequired) => {
 };
 
 const updateChart = (continent, data) => {
-  myChart.type = "line";
+  if (myChart.config.type === "pie") {
+    changeChartStyle("line");
+  }
   const countries = getCountriesByContinent(continent);
-  console.log(countries);
   myChart.data.labels = getDataByCountries(countries, "name");
   const latestCovidData = getDataByCountries(countries, "latest_data");
   myChart.data.datasets[0].data = getDataByCountries(latestCovidData, data);
   myChart.data.datasets[0].label = `covid 19 ${data} cases`;
   myChart.update();
+};
+
+const displayCountryChart = (country) => {
+  if (myChart.config.type === "line") {
+    changeChartStyle("pie");
+  }
+  myChart.data.labels = [
+    "confirmed",
+    "deaths",
+    "critical",
+    "recovered",
+    "new confirmed",
+    "new deaths",
+  ];
+  myChart.data.datasets[0].data = getCountryStats(country);
+  myChart.data.datasets[0].label = `${country.name} Covid Stats`;
+  myChart.update();
+};
+
+const getCountryStats = (country) => {
+  const statsArr = [];
+  statsArr.push(country.latest_data.confirmed);
+  statsArr.push(country.latest_data.deaths);
+  statsArr.push(country.latest_data.critical);
+  statsArr.push(country.latest_data.recovered);
+  statsArr.push(country.today.confirmed);
+  statsArr.push(country.today.deaths);
+  return statsArr;
+};
+// if we want to change the chart type we have to destroy the other one and create a new one with the desired type.
+const changeChartStyle = (chartType) => {
+  myChart.destroy();
+  myChart = new Chart(ctx, {
+    type: chartType,
+    data: {
+      labels: [],
+      datasets: [
+        {
+          label: "",
+          data: [],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+  });
 };
 
 // CHART
@@ -165,18 +226,3 @@ var myChart = new Chart(ctx, {
     },
   },
 });
-
-// to refactor with filter and map on object.keys(countriesFixedObj)
-//   asiaCountries = {};
-//   for (const countryCode of Object.keys(countriesFixedObj)) {
-//     if (countriesFixedObj[countryCode].region === "Asia") {
-//       asiaCountries[countriesFixedObj[countryCode].name] =
-//         countriesFixedObj[countryCode];
-//     }
-//   }
-//   console.log(asiaCountries);
-//   myChart.data.labels = Object.keys(asiaCountries);
-//   myChart.data.datasets[0].data = Object.keys(asiaCountries).map(
-//     (c) => asiaCountries[c].latest_data.confirmed
-//   );
-//   myChart.update();
